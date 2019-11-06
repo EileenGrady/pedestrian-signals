036af0c6b45a1558220c474e27a845cf04b6db81
Eileen Grady <eileen.grady@uky.edu>
2019-11-06 14:52:07 -0500

    try another package to find ped signals in council districts.

---
+++ scripts/point-in-polygon-2.js
@@ -0,0 +1,51 @@
+"use strict"
+
+var fs = require('fs');
+var inside = require('point-in-geopolygon');
+
+fs.readFile(__dirname + "/../data/council-districts.json", "utf8", (err, geojson) => {
+    
+    if (err) throw err;
+    var districts = JSON.parse(geojson)
+    // console.log(districts)
+    
+    fs.readFile(__dirname + "/../data/ped-signals-filtered.json", "utf8", (err, geojson2) => {
+      
+        if (err) throw err;
+        var signals = JSON.parse(geojson2)
+        countPoints(districts, signals)
+    })
+})
+
+function countPoints(districts, signals) {
+    // loop through each district
+    // start count at 0
+    // define polygon as the array of coordinates making up district shape
+    districts.features.forEach((feature) => {
+        let count = 0;
+        var polygon = feature.geometry.coordinates;
+     
+        // loop through each pedestrian signal
+        signals.features.forEach((row) => {
+            var y = row.properties.evnt_lat;
+            
+            // ran into null values, so this skips over any null values
+            // then define point as the signal point geometry
+            // test to see if the signal is inside the district polygon
+            // test returns boolean true or false
+            if (y === null) {
+                // console.log('skip')
+            } else {
+                var point = row.geometry.coordinates;
+                var result = inside.polygon(polygon, point);
+            }
+            // if test result is true, then increment count one
+            if (result === true) {
+                count++
+            }
+        })
+
+        feature.properties.count = count;
+        console.log(feature.properties.count)
+    })
+}
